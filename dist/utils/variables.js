16abbd89095be595df61b0853866b31435d4a778
Esraa Mohamed <esraa22m@gmail.com>
2026-07-07 22:10:48 +0300

    chore: add .gitignore, stop tracking node_modules/dist/.env

--- dist/utils/variables.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PORT = exports.MONGODB_URI = void 0;
-exports.MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/melo';
-exports.PORT = process.env.PORT || 3000;
